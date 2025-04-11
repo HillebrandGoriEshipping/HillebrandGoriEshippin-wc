ceaf6ddbd56b852a086da841206339ca69e55ddd
NicoExcalibur <nico.zanz@gmail.com>
2025-04-11 12:20:41 +0200

    basic structure

---
+++ HillebrandGoriEshipping.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hillebrand Gori eShipping
+*/
+
+require __DIR__ . '/vendor/autoload.php';
+
+use HGeS\App;
+
+
+new App();
---
+++ src/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HGeS;
+
+class App
+{
+    public function __construct()
+    {
+        exit('coucou');
+    }
+}
