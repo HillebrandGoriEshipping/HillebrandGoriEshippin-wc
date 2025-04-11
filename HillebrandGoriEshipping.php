c5b4f1a1bd2212bdb5964a8df55cd6a9200def03
NicoExcalibur <nico.zanz@gmail.com>
2025-04-11 15:09:47 +0200

    deleted phpunit

--- HillebrandGoriEshipping.php
+++ HillebrandGoriEshipping.php
@@ -1,7 +1,17 @@
 <?php
 /*
-Plugin Name: Hillebrand Gori eShipping
+ * Plugin Name: Hillebrand Gori eShipping
+ * Description: Hillebrand Gori eShipping plugin for WooCommerce
+ * Version: 2.0.0
+ * Author: Hillebrand Gori eSHipping
+ * Author URI: https://eshipping.hillebrandgori.com
+ * Requires Plugins: WooCommerce
+ * Requires PHP: 8.3
+ * License: GPLv3
 */
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
 require __DIR__ . '/vendor/autoload.php';
 
