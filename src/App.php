a7672d69dd85f1c4b9895e6f23b2108f1c00d002
GaetanTrema <gaetantrema@gmail.com>
2025-05-12 17:45:25 +0200

    add custom API endpoints class for proxy client

--- src/App.php
+++ src/App.php
@@ -4,6 +4,7 @@ namespace HGeS;
 
 use HGeS\Admin\Settings\Menu;
 use HGeS\Admin\Settings\SettingsController;
+use HGeS\Api\CustomEndpoints;
 use HGeS\Assets\Scripts;
 use HGeS\Assets\Styles;
 use HGeS\WooCommerce\ClassicUiRender;
@@ -31,6 +32,8 @@ class App
 
         add_action('wp_enqueue_scripts', [Scripts::class, 'enqueue']);
         add_action('wp_enqueue_scripts', [Styles::class, 'enqueue']);
+        add_action('rest_api_init', [CustomEndpoints::class, 'register']);
+        
         add_filter('woocommerce_package_rates', [ClassicUiRender::class, 'sortShippingMethods'], 10, 2);
         add_filter('woocommerce_cart_shipping_method_full_label', [ClassicUiRender::class, 'renderLabel'], 10, 2);
         add_filter('woocommerce_cart_shipping_packages', [ClassicUiRender::class, 'invalidateRatesCache'], 100);
