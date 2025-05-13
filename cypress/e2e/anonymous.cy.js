33d32fc173df1b114fcaacb1e9c900a528db2d22
GaetanTrema <gaetantrema@gmail.com>
2025-05-13 23:48:47 +0200

    add shipping rates selection

--- cypress/e2e/anonymous.cy.js
+++ cypress/e2e/anonymous.cy.js
@@ -8,7 +8,19 @@ describe('Cart spec', () => {
 
   it('Select a shipping method in cart view', () => {
     cy.visit('/index.php/cart');
-    const flatRateItem = cy.contains('Flat rate').should('be.visible');
-    flatRateItem.click();
+
+    const cartShippingFormButton = cy.get('.wc-block-components-totals-shipping-panel > div[role="button"]').should('be.visible');
+    cartShippingFormButton.click();
+    
+    cy.get('.wc-block-components-address-form__postcode input').type('45000');
+    cy.get('.wc-block-components-address-form__city input').type('Orléans');
+
+    cy.get('form.wc-block-components-shipping-calculator-address .wc-block-components-shipping-calculator-address__button').click();
+
+    const doorDeliverySectionHeader = cy.get('span').contains('Door Delivery').parents('button.accordion-header').should('be.visible');
+    doorDeliverySectionHeader.click();
+
+    const chrono18RateElement = cy.contains('Chrono 18H').should('be.visible');
+    chrono18RateElement.click();
   });
 });
