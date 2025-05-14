86e8aa721f8c50ea793c1cb2279416a72123b24a
GaetanTrema <gaetantrema@gmail.com>
2025-05-14 16:17:09 +0200

    complete anonymous cart test

--- cypress/e2e/anonymous.cy.js
+++ cypress/e2e/anonymous.cy.js
@@ -1,4 +1,5 @@
 import addToCart from "../support/addToCart";
+import { selectRateInAccordion } from "../support/shippingRates";
 
 describe('Cart spec', () => {
 
@@ -9,18 +10,16 @@ describe('Cart spec', () => {
   it('Select a shipping method in cart view', () => {
     cy.visit('/cart');
 
+    // Define a country and a city to enable the display of the shipping rates
     const cartShippingFormButton = cy.get('.wc-block-components-totals-shipping-panel > div[role="button"]').should('be.visible');
     cartShippingFormButton.click();
-    
     cy.get('.wc-block-components-address-form__postcode input').type('45000');
     cy.get('.wc-block-components-address-form__city input').type('Orléans');
-
     cy.get('form.wc-block-components-shipping-calculator-address .wc-block-components-shipping-calculator-address__button').click();
 
-    const doorDeliverySectionHeader = cy.get('span').contains('Door Delivery').parents('button.accordion-header').should('be.visible');
-    doorDeliverySectionHeader.click();
-
-    const chrono18RateElement = cy.contains('Chrono 18H').should('be.visible');
-    chrono18RateElement.click();
+    // select a shipping method
+    selectRateInAccordion('Door Delivery', 'Chrono 18');
+    // then another
+    selectRateInAccordion('Other shipping method', 'Flat rate');
   });
 });
