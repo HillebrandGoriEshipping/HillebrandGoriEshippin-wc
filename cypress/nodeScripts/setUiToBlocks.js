aa9005daafb85f4648fb5981930e442572447f3b
GaetanTrema <gaetantrema@gmail.com>
2025-05-15 12:23:05 +0200

    refactor db connection instanciation

--- cypress/nodeScripts/setUiToBlocks.js
+++ cypress/nodeScripts/setUiToBlocks.js
@@ -1,17 +1,9 @@
 console.log('Setting UI to blocks');
-const mysql = require('mysql2/promise');
 const runSqlDump = require('./runSqlDump');
+const getConnection = require('./dbConnection');
 
 async function update() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
-
-  await runSqlDump(connection, './cypress/fixtures/db/set-blocks-ui.sql');
+  await runSqlDump(await getConnection(), './cypress/fixtures/db/set-blocks-ui.sql');
 }
 
 update();
--- cypress/nodeScripts/setUiToClassic.js
+++ cypress/nodeScripts/setUiToClassic.js
@@ -1,17 +1,9 @@
 console.log('Setting UI to classic');
-const mysql = require('mysql2/promise');
 const runSqlDump = require('./runSqlDump');
+const getConnection = require('./dbConnection');
 
 async function update() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
-
-  await runSqlDump(connection, './cypress/fixtures/db/set-classic-ui.sql');
+  await runSqlDump(await getConnection(), './cypress/fixtures/db/set-classic-ui.sql');
 }
 
 update();
