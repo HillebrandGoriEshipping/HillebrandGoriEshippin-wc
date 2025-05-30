a391711239e9812ade52b35e8cc72b2406af32d9
GaetanTrema <gaetantrema@gmail.com>
2025-05-30 12:10:48 +0200

    test excise number presence

--- cypress/support/orderConfirmation.js
+++ cypress/support/orderConfirmation.js
@@ -6,8 +6,12 @@ export const checkOrderConfirmationContent = (isBusinessOrder) => {
         cy.get(`.woocommerce-column--${addressType}-address`).then(addressColumn => {
           cy.wrap(addressColumn).find('dt').contains('Business order').should('be.visible');
           cy.wrap(addressColumn).find('dd').contains('Yes').should('be.visible');
+
           cy.wrap(addressColumn).find('dt').contains('Company name').should('be.visible');
           cy.wrap(addressColumn).find('dd').contains('Test Company').should('be.visible');
+          
+          cy.wrap(addressColumn).find('dt').contains('Excise number').should('be.visible');
+          cy.wrap(addressColumn).find('dd').contains('12345678901234').should('be.visible');
         });
       });
     }
