de974bf38165f0c50bde6599908f62b0c8eca6f9
GaetanTrema <gaetantrema@gmail.com>
2025-05-30 11:19:37 +0200

    test

--- cypress/e2e/blocksUi/anon-order.cy.js
+++ cypress/e2e/blocksUi/anon-order.cy.js
@@ -34,15 +34,10 @@ describe('Block UI Order spec', () => {
     cy.get('#shipping-hges-is-company-address').click();
     cy.get('#shipping-hges-is-company-address').should('be.checked');
 
-    cy.get('#shipping-hges-company-name').should('be.visible');
-    cy.get('#shipping-hges-company-name').should('have.value', '');
-    cy.get('#shipping-hges-company-name').type('Test Company');
-    cy.get('#shipping-hges-company-name').should('have.value', 'Test Company');
-
     cy.get('button.wc-block-components-checkout-place-order-button').should('be.visible');
     cy.get('button.wc-block-components-checkout-place-order-button').click();
 
-    checkOrderConfirmationContent();
+    checkOrderConfirmationContent(false);
   });
 
 
@@ -99,5 +94,29 @@ describe('Block UI Order spec', () => {
       cy.get('button').contains('Place Order').click(); 
       cy.get('.woocommerce-column--shipping-address address').contains(thirdPickupPointName).should('be.visible');
     });
+  });
+
+  it('Saves custom buisiness order address fields', () => {
+    cy.visit('/checkout');
+
+    cy.get('.wc-block-components-address-form__email input').should('be.visible');
+    cy.get('.wc-block-components-address-form__email input').should('have.value', '');
+    cy.get('.wc-block-components-address-form__email input').type('test@test.com');
+
+    shippingAddressFormBlocksFill({
+      'shipping-first_name': 'Jean',
+      'shipping-last_name': 'Némar',
+      'shipping-address_1': '1 rue du Test Automatisé',
+      'shipping-postcode': '45000',
+      'shipping-city': 'Orléans',
+      'shipping-hges-is-company-address': true,
+      'shipping-hges-company-name': 'Test Company',
+      'shipping-hges-excise-number': '12345678901234',
+    });
+
+    cy.get('button.wc-block-components-checkout-place-order-button').should('be.visible');
+    cy.get('button.wc-block-components-checkout-place-order-button').click();
+
+    checkOrderConfirmationContent(true);
   });
 });
