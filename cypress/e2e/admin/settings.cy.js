d8a0cd5f81da7d965271ed81415733d681be7611
GaetanTrema <gaetantrema@gmail.com>
2025-05-30 16:54:26 +0200

    check seller address

--- cypress/e2e/admin/settings.cy.js
+++ cypress/e2e/admin/settings.cy.js
@@ -22,4 +22,18 @@ describe('Admin HGeS settings page spec', () => {
         cy.get('#apikey-settings-form').submit();
          cy.get('#api-input').should('have.value', '+jc0wVA/fprjeEcbG6gWcl4FNhMJCTs3Y29VeysSb5PNvAxJPERBjfL/RG1736wvEkg=');
     });
+
+    it ('Configuration form', () => {
+        cy.visit('/wp/wp-admin/admin.php?page=hillebrand-gori-eshipping');
+        cy.get('#address-table').should('be.visible');
+        cy.get('#address-table').find('tbody tr').then((rows) => {
+            cy.wrap(rows).eq(0).find('td').contains('Adresse principale').should('be.visible');
+            cy.wrap(rows).eq(1).find('td').contains('MY COMPANY').should('be.visible');
+            cy.wrap(rows).eq(2).find('td').contains('John DOE').should('be.visible');
+            cy.wrap(rows).eq(3).find('td').contains('33102030405').should('be.visible');
+            cy.wrap(rows).eq(4).find('td').contains('69B rue du Colombier').should('be.visible');
+            cy.wrap(rows).eq(5).find('td').contains('Orléans').should('be.visible');
+            
+        });
+    });
 });
