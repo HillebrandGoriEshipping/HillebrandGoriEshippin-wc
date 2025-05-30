e5224067a74534703ab89a89af49cca2f4045f45
GaetanTrema <gaetantrema@gmail.com>
2025-05-30 16:15:15 +0200

    doc blocks

--- src/Assets/FrontendValidator.php
+++ src/Assets/FrontendValidator.php
@@ -5,8 +5,20 @@ namespace HGeS\Assets;
 use HGeS\Utils\Enums\OptionEnum;
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * Class FrontendValidator
+ * This class is used to retrieve the validation constraints for the frontend.
+ * It extracts the constraints from the different enums available
+ */
 class FrontendValidator
 {
+    /**
+     * Get the constraints for a specific enum class.
+     * The enum class must implement the getList() and getConstraints() methods.
+     *
+     * @param string $enum The enum class name
+     * @return array An associative array of option names and their constraints
+     */
     public static function getConstraintsByEnum(string $enum): array
     {
         if (class_exists($enum)) {
@@ -26,13 +38,20 @@ class FrontendValidator
 
                 $optionConstraints[$option] = $constraintsWithKeys;
             }
-            
+
             return $optionConstraints;
         } else {
             return [];
         }
     }
 
+    /**
+     * Get all the constraints for the frontend.
+     * This method retrieves the constraints for all enums that are relevant for the frontend.
+     * Here, we list the enums that are used for validation in the frontend by adding a new entry in the $constraints array.
+     *
+     * @return array An associative array of option names and their constraints
+     */
     public static function getAll(): array
     {
         $constraints = [];
