22cb6aa2b81442b6a82f5ca581d269084a678340
GaetanTrema <gaetantrema@gmail.com>
2025-06-27 10:42:11 +0200

    docblocks and typehinting

--- assets/js/orderEditPage.js
+++ assets/js/orderEditPage.js
@@ -7,10 +7,10 @@ const orderEditPage = {
         this.changeShippingRateButton = document.querySelector('#hges-change-shipping-rate-button');
         this.closeShippingRateModalButton = document.querySelector('#hges-shipping-rate-modal .modal__close');
         if (this.changeShippingRateButton) {
-            this.changeShippingRateButton.addEventListener('click', this.openShippingRateModal);
+            this.changeShippingRateButton.addEventListener('click', this.openShippingRateModal.bind(this));
         }
         if (this.closeShippingRateModalButton) {
-            this.closeShippingRateModalButton.addEventListener('click', this.closeShippingRateModal);
+            this.closeShippingRateModalButton.addEventListener('click', this.closeShippingRateModal.bind(this));
         }
     },
     async openShippingRateModal() {
@@ -20,12 +20,21 @@ const orderEditPage = {
             const currentUrlParams = new URLSearchParams(window.location.search);
             const response = await apiClient.get('/shipping-rates', {orderId: currentUrlParams.get('id')}, {}, true);
             document.querySelector('#hges-shipping-rate-modal .shipping-rate-list').innerHTML = response.shippingRatesHtml;
+            document.querySelectorAll('#hges-shipping-rate-modal .shipping-rate-list .hges-shipping-method').forEach((rateElement) => {
+                rateElement.addEventListener('click', this.onShippingRateSelected.bind(this));
+            });
         }
     },
     closeShippingRateModal() {
         const modal = document.querySelector('#hges-shipping-rate-modal');
         if (modal) {
             modal.classList.add('hidden');
+        }
+    },
+    onShippingRateSelected(event) {
+        const shippingRateChecksum = event.target.dataset.checksum;
+        if (shippingRateChecksum) {
+            console.log('Selected shipping rate checksum:', shippingRateChecksum);
         }
     }
 };
--- src/Admin/Order/ShippingMethodRow.php
+++ src/Admin/Order/ShippingMethodRow.php
@@ -7,12 +7,16 @@ use HGeS\Utils\Messages;
 use HGeS\Utils\Twig;
 use HGeS\WooCommerce\Model\ShippingMethod;
 
+/**
+ * Handles the display of the shipping method row in the WooCommerce order admin page
+ */
 class ShippingMethodRow {
     
     /**
      * Initialize the admin hooks and filters for the shipping method row
      */
-    public static function initAdmin() {
+    public static function initAdmin()
+    {
         add_action('woocommerce_before_order_itemmeta', [self::class, 'beforeOrderItemMeta'], 10, 3);
         add_action('woocommerce_after_order_itemmeta', [self::class, 'afterOrderItemMeta'], 10, 3);
         add_filter('woocommerce_hidden_order_itemmeta', [self::class, 'hiddenOrderItemMeta'], 10, 1);
@@ -21,8 +25,14 @@ class ShippingMethodRow {
     /**
      * Displays the shipping method edition button
      * triggered by the 'woocommerce_before_order_itemmeta' action
+     * @param int $item_id The item ID
+     * @param \WC_Order_Item $item The order item object
+     * @param \WC_Product|null $product The product object, if available
+     * 
+     * @return void
      */
-    public static function beforeOrderItemMeta(int $item_id, \WC_Order_Item $item, ?\WC_Product $product = null) {
+    public static function beforeOrderItemMeta(int $item_id, \WC_Order_Item $item, ?\WC_Product $product = null): void
+    {
         if (
             get_class($item) !== 'WC_Order_Item_Shipping' 
             || $item->get_data()['method_id'] !== ShippingMethod::METHOD_ID
@@ -59,16 +69,26 @@ class ShippingMethodRow {
     /**
      * Closes the div opened in beforeOrderItemMeta
      * triggered by the 'woocommerce_after_order_itemmeta' action
+     * 
+     * @param int $item_id The item ID
+     * @param \WC_Order_Item $item The order item object
+     * @param \WC_Product|null $product The product object, if available
+     * @return void
      */
-    public static function afterOrderItemMeta($item_id, $item, $order) {
+    public static function afterOrderItemMeta(int $item_id, \WC_Order_Item $item, ?\WC_Product $product = null): void
+    {
         echo '</div>';
     }
 
     /**
      * Hide all the meta data related to the shipping method so they can't be edited one by one
      * triggered by the 'woocommerce_hidden_order_itemmeta' filter
+     * 
+     * @param array $hidden The array of hidden meta keys
+     * @return array The modified array of hidden meta keys
      */
-    public static function hiddenOrderItemMeta($hidden) {
+    public static function hiddenOrderItemMeta(array $hidden): array
+    {
         $hidden[] = 'deliveryDate';
         $hidden[] = 'pickupDate';
         $hidden[] = 'insurancePrice';
