879a60bb1417671f3171450cddbb9f5c587b463f
GaetanTrema <gaetantrema@gmail.com>
2025-07-09 16:18:23 +0200

    remove temp upload route

--- src/Router.php
+++ src/Router.php
@@ -49,25 +49,6 @@ class Router {
             SettingsController::saveApiKey();
             exit;
         }
-
-        if (isset($_GET['action'])
-            && $_GET['action'] === 'hges_upload_documents'
-            &&  $_SERVER['REQUEST_METHOD'] === 'POST'
-        ) {
-            $baseDir = $_SERVER['DOCUMENT_ROOT'] . '/content/uploads/tmp/';
-            if (!is_dir($baseDir)) {
-                mkdir($baseDir, 0777, true);
-            }
-            foreach($_FILES as $key => $file) {
-                $filename = $baseDir . uniqid().$file['name'];
-                $write = file_put_contents(
-                    $filename,
-                    file_get_contents($file['tmp_name'])
-                );
-                var_dump($write);
-            }
-            exit;
-        }
     }
 
     public static function ajaxRouter(): void
