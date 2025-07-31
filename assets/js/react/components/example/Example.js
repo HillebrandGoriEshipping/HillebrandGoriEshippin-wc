11cbe00f4d896dc6a7aa0783570bddd7c148f2ed
GaetanTrema <gaetantrema@gmail.com>
2025-07-31 10:09:26 +0200

    update example

--- assets/js/react/components/example/Example.js
+++ assets/js/react/components/example/Example.js
@@ -1,7 +1,13 @@
+/*
+  To inject this component into the DOM with Twig, you can use the following code snippet:
+  {{ spawnComponent('Example', {'foo': 'Bar'}) }}
+  The component has to be declared in the assets/js/react/components.js file.
+*/
+
 const Example = (props) => {
   return (
     <div>
-      <h3>React Component Example: </h3>
+      <h3>React Component Example</h3>
       <p>Foo: {props.foo}</p>
     </div>
   );
