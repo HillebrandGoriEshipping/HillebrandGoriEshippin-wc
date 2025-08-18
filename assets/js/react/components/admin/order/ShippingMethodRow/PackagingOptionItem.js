14cca6043ac7b544b6a1763bf30b0451430cc309
GaetanTrema <gaetantrema@gmail.com>
2025-08-18 11:43:49 +0200

    fix current value selected attribute in packaging select

--- assets/js/react/components/admin/order/ShippingMethodRow/PackagingOptionItem.js
+++ assets/js/react/components/admin/order/ShippingMethodRow/PackagingOptionItem.js
@@ -4,7 +4,7 @@ const PackagingOptionItem = ({ packageItem, packagingOptions, onSelect, onRemove
     const onSelectChange = (e) => {
         onSelect(packagingOptions.find(option => option.id === Number(e.target.value)) );
     };
-
+console.log('packageItem', packageItem);
     return (
         <div className="packaging-option-item">
             <h4>Package {packageItem.index}</h4>
@@ -13,7 +13,10 @@ const PackagingOptionItem = ({ packageItem, packagingOptions, onSelect, onRemove
             </div>
             <div>
                 <label>{__('Type')}:</label> 
-                <select value={packageItem.type} onChange={onSelectChange}>
+                <select 
+                    value={packageItem.id} 
+                    onChange={onSelectChange}
+                >
                     {packagingOptions.map((option) => (
                         <option key={option.id} value={option.id}>
                             {option.itemNumber}x{option.containerType} ({option.width}x{option.height}x{option.length} cm)
